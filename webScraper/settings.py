2bff512cd2b469bfbb85a24871cf598f4eec8a56
unknown <m_belkahla@gmail.com>
2020-08-28 14:25:47 +0200

    fixces

--- webScraper/settings.py
+++ webScraper/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import pusher
-import urlparse
+from urllib.parse as urlparse
 import redis 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
